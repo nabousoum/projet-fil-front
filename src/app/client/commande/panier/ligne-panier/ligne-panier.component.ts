b8d32cfa18827dbf5e68870beef26d07d820fe56
nabousoum <seynabousoumare98@outlook.fr>
2022-07-25 21:42:56 +0000

    modifs

--- src/app/client/commande/panier/ligne-panier/ligne-panier.component.ts
+++ src/app/client/commande/panier/ligne-panier/ligne-panier.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ss-ligne-panier',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ligne-panier.component.css']
 })
 export class LignePanierComponent implements OnInit {
-
+  @Input() isPanier = true;
   constructor() { }
 
   ngOnInit(): void {
